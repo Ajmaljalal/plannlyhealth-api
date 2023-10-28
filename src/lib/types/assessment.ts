5f3ea6ebaffd622cc0b32b0073887689d56a8db0
ajmaljalal <jalalajmal96@gmail.com>
2023-10-28 12:30:52 -0700

    risk profile type udates

--- src/lib/types/assessment.ts
+++ src/lib/types/assessment.ts
@@ -10,6 +10,10 @@ export type Question = {
 export type RiskProfile = {
   user_id: string;
   company_id: string;
+  user_job_title: string;
+  user_department: string;
+  user_birthday: string;
+  user_marital_status: string;
   assessment_date: string;
   risk_summary: Record<string, number>;
   detailed_breakdown: Record<string, { total_score: number; number_of_symptoms: number; key_responses: any[] }>;
